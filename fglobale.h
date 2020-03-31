545b59e8bdef69acf17f862d11a86a6218618e7d
Iustina Filipescu <62960885+iustinafilipescu@users.noreply.github.com>
2020-03-31 22:51:56 +0300

    Delete fglobale.h

--- fglobale.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-int CMate(Student a, Student b);
-int CIstorie(Student a, Student b);
-int CEngleza(Student a, Student b);
-int CMedie(Student a, Student b);
-int CNume(Student a, Student b);
-
